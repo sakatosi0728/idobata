8a0a8993ee6fb991768e59963a8bb44bd8a739ae
sakatosi0728 <sakatosi0728@gmail.com>
2020-10-12 12:11:03 +0900

    ルームの表示

--- db/migrate/20201012024948_create_rooms.rb
+++ db/migrate/20201012024948_create_rooms.rb
@@ -1,7 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-
+      t.string :name
+      t.string :text
+      t.text :image
       t.timestamps
     end
   end
